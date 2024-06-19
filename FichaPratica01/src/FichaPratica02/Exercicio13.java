97036db05d103955254c397e47d6c28005d01154
ArthurSSR-alt <ramalhoarthur09@gmail.com>
2024-06-19 19:33:36 -0300

    CESAE

--- FichaPratica01/src/FichaPratica02/Exercicio13.java
+++ FichaPratica01/src/FichaPratica02/Exercicio13.java
@@ -1,4 +1,39 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Exercicio13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+         int horas, minutos;
+
+        System.out.print("Insira as horas: ");
+        horas = input.nextInt();
+        System.out.print("Insira as minutos: ");
+        minutos = input.nextInt();
+
+
+        if (horas>=12) { //Tarde (PM)
+
+            if (horas != 12) {
+                horas = horas - 12;
+            }
+
+
+            System.out.println(horas + ":" + minutos + "PM");
+
+        }else{
+
+            System.out.println(horas + ":"+minutos+"AM");
+
+
+
+
+        }
+
+
+
+
+    }
 }
